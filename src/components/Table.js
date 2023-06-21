5b5c64ddb792285841859d342bc5a4a850adb91b
Thapelo <selebedi.thapelo6@gmail.com>
2023-06-21 15:41:43 +1200

    Fixed Images

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState, useMemo } from 'react'
 import PropTypes from 'prop-types';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -81,8 +82,8 @@ function EnhancedTableHead(props) {
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
+            align={'center'}
+            padding={'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
@@ -111,6 +112,7 @@ EnhancedTableHead.propTypes = {
 };
 
 export default function EnhancedTable() {
+  const [rows, setRows] = useState([])
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
@@ -118,10 +120,10 @@ export default function EnhancedTable() {
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [products, setProducts] = React.useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch('https://app.spiritx.co.nz/api/products')
     .then((response) => response.json())
-    .then((data) => setProducts(data));
+    .then((data) => setRows(data));
   }, []);
 
   const handleRequestSort = (event, property) => {
@@ -129,6 +131,26 @@ export default function EnhancedTable() {
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name)
+    let newSelected = []
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name)
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1))
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1))
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      )
+    }
+
+    setSelected(newSelected)
+  }
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -141,50 +163,74 @@ export default function EnhancedTable() {
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, products.length - page * rowsPerPage);
 
+  const visibleRows = useMemo(
+    () =>
+      rows
+        .sort(getComparator(order, orderBy))
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [rows, order, orderBy, page, rowsPerPage]
+  )
+
   return (
-    <Paper sx={{ width: '100%', mb: 2 }}>
-      <TableContainer>
-        <Table
-          sx={{ minWidth: 750 }}
-          aria-labelledby="tableTitle"
-        >
-          <EnhancedTableHead
-            order={order}
-            orderBy={orderBy}
-            onRequestSort={handleRequestSort}
-          />
-          <TableBody>
-            {stableSort(products, getComparator(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((product) => (
-                <TableRow
-                  hover
-                  tabIndex={-1}
-                  key={product.id}
-                >
-                  <TableCell align="left">{product.title}</TableCell>
-                  <TableCell align="left">{product.description}</TableCell>
-                  <TableCell align="right">{product.price}</TableCell>
-                  <TableCell align="right">{product.product_image}</TableCell>
+    <Box sx={{ maxWidth: '1200px', margin: '0 auto' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby='tableTitle'>
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                return (
+                  <TableRow
+                    hover
+                    onClick={(event) => handleClick(event, row.name)}
+                    tabIndex={-1}
+                    key={row.id}
+                    sx={{ cursor: 'pointer' }}
+                  >
+                    <TableCell component='th' scope='row' padding='normal' align='center'>
+                      {row.title}
+                    </TableCell>
+                    <TableCell align='center'>{row.price}</TableCell>
+                    <TableCell align='center'>{row.description}</TableCell>
+                    <TableCell align='center'>
+                      {row.product_image ? (
+                        <img
+                          src={`https://app.spiritx.co.nz/storage/${row.product_image}`}
+                          alt={row.titile}
+                          width='130px'
+                        />
+                      ) : (
+                        'No image'
+                      )}
+                    </TableCell>
+                    <TableCell align='center'></TableCell>
+                  </TableRow>
+                )
+              })}
+              {emptyRows > 0 && (
+                <TableRow>
+                  <TableCell colSpan={6} />
                 </TableRow>
-              ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={products.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component='div'
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+    
   );
 }
