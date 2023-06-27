edad3705dda50f97fd11472a456f2180d35a21fc
Thapelo <selebedi.thapelo6@gmail.com>
2023-06-27 23:57:48 +1200

    signin logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import React, { useState } from 'react';
 import Table from './components/Table';
+import SignIn from './components/SignIn';
 
 function App() {
+
+  const [signInSuccess, setSignInSuccess] = useState(false);
+
+  function handleSigInSuccess() {
+    setSignInSuccess(true);
+  }
   return (
     <div>
-      <Table />
+      {signInSuccess ? (
+        <Table />
+      ) : (
+        <SignIn onSignInSuccess={handleSigInSuccess} />
+      )}
     </div>
   );
 }
